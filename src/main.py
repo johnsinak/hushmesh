5363adb77884a49e81325d256c9e4410413d6b5f
sinku <sininoir@gmail.com>
2024-06-02 02:52:22 -0400

    Add new settings

--- src/main.py
+++ src/main.py
@@ -2,5 +2,14 @@ from MeshSim import MeshSim
 from settings import *
 
 if __name__ == "__main__":
-    sim = MeshSim(T, N, WORLD_DIMENSION, MOVE_RANGE, MESSAGE_EXCHANGE_RANGE)
+    sim = MeshSim(
+        T,
+        N,
+        WORLD_DIMENSION,
+        MOVE_RANGE,
+        MESSAGE_EXCHANGE_RANGE,
+        ADVERSARY_RATIO,
+        WS_DELTA,
+        WS_BETA,
+    )
     sim.run()
--- src/settings.py
+++ src/settings.py
@@ -1,10 +1,14 @@
-T = 100 
+T = 100
 N = 200
 A = 100
 B = 100
-WORLD_DIMENSION = (A,B) 
+WORLD_DIMENSION = (A, B)
 r = 1
-MOVE_RANGE = ((-r,r), (-r,r))
+MOVE_RANGE = ((-r, r), (-r, r))
 d = 1
 MESSAGE_EXCHANGE_RANGE = ((-d, d), (-d, d))
 ADVERSARY_RATIO = 0.01
+
+# Watts-Strogatz model parameters
+WS_DELTA = N // 10
+WS_BETA = 0.5
