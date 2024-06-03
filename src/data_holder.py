b90d81903867fc63433452779c092e491dbf7665
sinku <sininoir@gmail.com>
2024-06-03 19:09:06 -0400

    Add the data holder file

---
+++ src/data_holder.py
@@ -0,0 +1,18 @@
+from settings import T
+global misinformation_count, upvoted_misinformation_count, messages_exchanged_steps, votes_exchanged_steps, message_propagation_times_80_percentile, message_propagation_times_90_percentile, message_propagation_times_full, total_owt_created, total_owts_responded_to, adversary_count
+misinformation_count = [0] * T
+upvoted_misinformation_count = [0] * T
+downvoted_misinformation_count = [0] * T
+
+messages_exchanged_steps = [0] * T
+votes_exchanged_steps = [0] * T 
+
+message_propagation_times_80_percentile = []
+message_propagation_times_90_percentile = []
+message_propagation_times_full = []
+
+# total messages sent out
+total_owt_created = 0
+total_owts_responded_to = 0
+
+adversary_count = 0
