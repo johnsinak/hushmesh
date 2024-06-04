0b67fa1719382995d6bad8dd48a35e3a293ccbc9
sinku <sininoir@gmail.com>
2024-06-04 11:04:20 -0400

    Add new data entries

--- src/data_holder.py
+++ src/data_holder.py
@@ -1,5 +1,5 @@
 from settings import T
-global misinformation_count, upvoted_misinformation_count, messages_exchanged_steps, votes_exchanged_steps, message_propagation_times_80_percentile, message_propagation_times_90_percentile, message_propagation_times_full, total_owt_created, total_owts_responded_to, adversary_count
+global misinformation_count, upvoted_misinformation_count, messages_exchanged_steps, votes_exchanged_steps, message_propagation_times_80_percentile, message_propagation_times_90_percentile, message_propagation_times_full, total_owt_created, total_owts_responded_to, adversary_count, message_seen_counter
 misinformation_count = [0] * T
 upvoted_misinformation_count = [0] * T
 downvoted_misinformation_count = [0] * T
@@ -16,3 +16,7 @@ total_owt_created = 0
 total_owts_responded_to = 0
 
 adversary_count = 0
+
+highest_percentile_reached_for_message = 0
+
+message_seen_counter = {}
